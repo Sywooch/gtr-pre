d1b3442e2e46f8798637827cd2546194e6c8e674
mastuyink <aziest99@gmail.com>
2017-10-13 12:54:12 +0700

    next

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -289,7 +289,6 @@ $customScript = <<< SCRIPT
         $("html, body").animate({ scrollTop: 0 }, 600); 
         return false; 
     }); 
-    
 SCRIPT;
 $this->registerJs($customScript, \yii\web\View::POS_READY);
 ?>
