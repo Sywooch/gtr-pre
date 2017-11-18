7a043eae778a7a875e41247707d69b5ff81b236a
mastuyink <aziest99@gmail.com>
2017-11-18 19:01:39 +0800

    design galery part night

--- frontend/views/content/content.php
+++ frontend/views/content/content.php
@@ -14,11 +14,11 @@ $this->registerMetaTag([
 
 ?>
 <?php foreach ($listContent as $keyAr => $valContent): ?>
-<div class=" col-sm-6 col-md-4">
+<div class="col-md-4 col-sm-6 col-xs-12">
 
     <div class="iam thumbnail material-card">
         <div class="material-card__header">
-        <?= Html::a(Html::img(['thumbnail','slug'=>$valContent->slug], ['alt'=>'thumbnail'.$valContent->slug]),['/content/view','slug'=>$valContent->slug,]); ?>
+        <?= Html::a(Html::img(['thumbnail','slug'=>$valContent->slug], ['class'=>'img-thumb img-responsive','alt'=>'thumbnail'.$valContent->slug]),['/content/view','slug'=>$valContent->slug,]); ?>
         </div>
     <div class="material-card__content">
         <h5 class="material-card__title"><?= Html::a($valContent->title, ['view','slug'=>$valContent->slug]); ?></h5>
@@ -32,24 +32,21 @@ $this->registerMetaTag([
 </div>
 <?php endforeach;	?>
 <?php
-$customCss = <<< SCRIPT
-    .material-card__header{
+
+?>
+<style type="text/css">
+     .material-card__header{
         width: 350px;
         height: 200px;
-        position: relative;
-        display: inline-block;
-        overflow: hidden;
-        margin: 0;
     }
-    .material-card__header > img{
+    .img-thumb{
+        box-sizing: border-box;
+        max-width: 350px;
+        max-height: 200px;
+        height: 200px;
         display: block;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        right: 50%;
-        min-height: 100%;
-        min-width: 100%;
-        transform: translate(-50%, -50%);
+        width: 100%;
+        overflow: hidden;
     }
     .iam{
         min-height: 425px;
@@ -59,6 +56,4 @@ $customCss = <<< SCRIPT
         min-height: 140px;
         height: auto;
     }
-SCRIPT;
-$this->registerCss($customCss);
-?>
+</style>
