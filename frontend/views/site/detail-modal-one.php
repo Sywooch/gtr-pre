eab48ebfec7447d6beddf55ec0fe9c5dd65cd222
mastuyink <aziest99@gmail.com>
2017-12-19 19:11:15 +0800

    last minute booking

--- frontend/views/site/detail-modal-one.php
+++ frontend/views/site/detail-modal-one.php
@@ -2,8 +2,8 @@
 use yii\helpers\Html;
 
 
-$aD = $tripDeparture->adult_price/$currency->kurs*$paxAdult;
-$cD = $tripDeparture->child_price/$currency->kurs*$paxChild;
+$aD = $tripDeparture['adult_price']/$currency['kurs']*$paxAdult;
+$cD = $tripDeparture['child_price']/$currency['kurs']*$paxChild;
 $totalD = $aD+$cD;
 ?>
 
@@ -13,37 +13,37 @@ $totalD = $aD+$cD;
             <div class="col-md-2">
             <b>Departure</b>
               <br>
-              <?= date('D, d-M-Y',strtotime($tripDeparture->date)) ?>
+              <?= date('D, d-M-Y',strtotime($tripDeparture['date'])) ?>
             </div>
             <div class="col-md-2">
             <b>Boat</b><br>
-              <?= $tripDeparture->idBoat->idCompany->name ?>       
+              <?= $tripDeparture['idBoat']['idCompany']['name'] ?>       
             </div>
             <div class="col-md-2">
             <b>Dept Time</b><br>
-            <?= date('H:i',strtotime($tripDeparture->dept_time)) ?>
+            <?= date('H:i',strtotime($tripDeparture['dept_time'])) ?>
               
             </div>
             <div class="col-md-4">
             <b>Route</b><br>
-            <?= $tripDeparture->idRoute->departureHarbor->name." -> ".$tripDeparture->idRoute->arrivalHarbor->name ?><br>
-            (<?= $tripDeparture->idEstTime->est_time ?>)
+            <?= $tripDeparture['idRoute']['departureHarbor']['name']." -> ".$tripDeparture['idRoute']['arrivalHarbor']['name'] ?><br>
+            (<?= $tripDeparture['idEstTime']['est_time'] ?>)
               </div>
             
            <div class="col-md-2">
             
-           <b class="text-strong"><?php $currency->currency." ".round($totalD,0,PHP_ROUND_HALF_UP) ?></b>
-           <?= Html::img(['/site/logo','id'=>$tripDeparture->idBoat->id_company], ['class'=>'boat-logo-modal']) ?>
+           <b class="text-strong"><?php $currency['currency']." ".round($totalD,0,PHP_ROUND_HALF_UP) ?></b>
+           <?= Html::img(['/site/logo','id'=>$tripDeparture['idBoat']['id_company']], ['class'=>'boat-logo-modal']) ?>
            </div>
-</div>
+        </div>
      
     </div>
 </div>
 
 <span class="pull-right">
 <button id="btn-close-modal" type="button" class="btn material-btn material-btn_danger main-container__column material-btn_lg" data-dismiss="modal">Cancel</button>
-<b class="harga-total"><?= $currency->currency." ".round($totalD,0,PHP_ROUND_HALF_UP); ?></b>&nbsp
-<?= Html::a('Book', ['/book/add-to-cart', 'tripDeparture' => $tripDeparture->id], [
+<b class="harga-total"><?= $currency['currency']." ".round($totalD,0,PHP_ROUND_HALF_UP); ?></b>&nbsp
+<?= Html::a('Book', ['/book/add-to-cart', 'tripDeparture' => $tripDeparture['id']], [
             'class' => 'btn material-btn material-btn_warning main-container__column material-btn_lg',
             'data' => [
                 'method' => 'post',
